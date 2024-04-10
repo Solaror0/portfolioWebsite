2fa2c33ec1caeed9b3d62d7b2f48d798e34e5e76
Solaror0 <133717438+Solaror0@users.noreply.github.com>
2024-04-09 21:25:53 -0400

    Moving flask into backend folder

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -1,6 +1,6 @@
 from flask import Flask
 
-from config import Config
+from backend.config import Config
 
 def create_app(config_class=Config):
     app = Flask(__name__)
@@ -13,8 +13,8 @@ def create_app(config_class=Config):
     from app.main import bp as main_bp
     app.register_blueprint(main_bp)
 
-    @app.route('/test/')
-    def test_page():
+    @app.route('/')
+    def serve():
         return '<h1>Testing the Flask Application Factory Pattern</h1>'
 
 
